2c99cd6489cb17cb29278ed4ebec4227dd30daca
Inge Dalby <inge.dalby@gmail.com>
2019-06-11 17:35:30 +0200

    1.0!
    These changes brings the addon to 1.0!
    - Added ability to unlock the bar, allowing users to put it where ever they want to. This is saved
    - Changed setting menu
    - Added ability to reset the bars position
    - Added an 'always show' toggle

--- TMPBar/TMPBar.lua
+++ TMPBar/TMPBar.lua
@@ -9,22 +9,26 @@ TMPBar.playerName = ""
 TMPBar.goldIcon = zo_iconFormat("esoui/art/currency/currency_gold.dds", 16, 16)
 TMPBar.bagIcon = zo_iconFormat("esoui/art/tooltips/icon_bag.dds", 16, 16)
 TMPBar.bankBagIcon = zo_iconFormat("esoui/art/icons/servicemappins/servicepin_bank.dds", 16, 16)
+TMPBar.lockWindow = true
 
 TMPBar.savedVariables = nil
-TMPBar.version = 4
+TMPBar.version = 5
 TMPBar.default = {
 	BarLocation = "BOTTOMLEFT",
 	warningWhen = 15,
 	normalColor = { 255, 255, 255, 1 },
 	warningColor = { 236, 202, 0, 1 },
 	fullColor = { 230, 0, 0, 1 },
-	showBankBag = true
+	showBankBag = true,
+	alwaysShow = false
 }
 
 function TMPBar.Init()
 	TMPBar.playerName = GetUnitName("player")
 	TMPBar.gold = GetCurrentMoney("player")
 	TMPBar.savedVariables = ZO_SavedVars:New("TMPBarVars", TMPBar.version, nil, TMPBar.default)
+	TMPBarWindow:SetMovable(not TMPBar.lockWindow)
+	TMPBarWindow:SetMouseEnabled(not TMPBar.lockWindow)
 
 	TMPBar.SetBagSlots()
 	TMPBar.SetBankBagSlots()
@@ -41,6 +45,14 @@ function TMPBar.Init()
 	EVENT_MANAGER:RegisterForEvent(TMPBar.name, EVENT_INVENTORY_ITEM_USED, TMPBar.SetBagSlots)
 	EVENT_MANAGER:RegisterForEvent(TMPBar.name, EVENT_GAME_CAMERA_UI_MODE_CHANGED, TMPBar.UIChanged)
 	EVENT_MANAGER:RegisterForEvent(TMPBar.name, EVENT_CLOSE_BANK, TMPBar.SetBankBagSlots)
+
+	TMPBarWindow:SetHandler("OnMoveStop", function(control)
+		local x, y = control:GetScreenRect()
+		TMPBar.savedVariables.window.x = x
+		TMPBar.savedVariables.window.y = y
+	end)
+
+	TMPBar.SetWindowPosition()
 end
 
 function TMPBar.OnAddOnLoaded(event, addonName)
@@ -83,10 +95,24 @@ function TMPBar.UpdateGoldAmount(eventCode, newMoney, oldMoney, reason)
 end
 
 function TMPBar.UIChanged(eventCode)
-	if IsGameCameraUIModeActive() then
+	if TMPBar.savedVariables.alwaysShow then
+		return
+	end
+
+	if IsGameCameraUIModeActive() and TMPBar.lockWindow then
 		TMPBarWindow:SetHidden(true)
 	else
 		TMPBarWindow:SetHidden(false)
+	end
+end
+
+function TMPBar.SetWindowPosition()
+	if TMPBar.savedVariables.window ~= nil then
+		TMPBarWindow:ClearAnchors()
+		TMPBarWindow:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, TMPBar.savedVariables.window.x, TMPBar.savedVariables.window.y)
+	else
+		local x, y = TMPBarWindow:GetScreenRect()
+		TMPBar.savedVariables.window = { x=x, y=y }
 	end
 end
 
@@ -110,66 +136,112 @@ function TMPBar.CreateSettingsWindow()
 	local optionsData = {
 		[1] = {
 			type = "header",
-			name = "TMPBar Settings",
+			name = "General",
 		},
 		[2] = {
-			type = "description",
-			text = "Basic settings for the TMPBar addon",
+			type = "checkbox",
+			name = "Lock",
+			tooltip = "Unlock to position manually",
+			default = true,
+			getFunc = function ()
+						return TMPBar.lockWindow
+					end,
+			setFunc = function (newValue)
+						TMPBar.lockWindow = newValue
+						TMPBarWindow:SetMovable(not TMPBar.lockWindow)
+						TMPBarWindow:SetMouseEnabled(not TMPBar.lockWindow)
+
+						if TMPBar.savedVariables.alwaysShow then
+							return
+						end
+
+						if not TMPBar.lockWindow then
+							TMPBarWindow:SetHidden(false)
+						else
+							TMPBarWindow:SetHidden(true)
+						end
+					end
 		},
 		[3] = {
+			type = "checkbox",
+			name = "Always show",
+			default = false,
+			getFunc = function ()
+						return TMPBar.savedVariables.alwaysShow
+					end,
+			setFunc = function (newValue)
+						TMPBar.savedVariables.alwaysShow = newValue
+						TMPBarWindow:SetHidden(not TMPBar.savedVariables.alwaysShow)
+					end,
+		},
+		[4] = {
+			type = "button",
+			name = "Reset position",
+			tooltip = "Resets the position of TMPBar",
+			func = function ()
+					TMPBarWindow:ClearAnchors()
+					TMPBarWindow:SetAnchor(LEFT, GuiRoot, BOTTOMLEFT, 10, -15)
+					TMPBar.savedVariables.window = nil
+				end,
+		},
+		[5] = {
+			type = "header",
+			name = "Bag options",
+		},
+		[6] = {
 			type = "checkbox",
 			name = "Show bank bag",
 			tooltip = "Determine wether or not your bank bag is shown",
 			default = true,
-			getFunc = function () return TMPBar.savedVariables.showBankBag end,
+			getFunc = function ()
+						return TMPBar.savedVariables.showBankBag
+					end,
 			setFunc = function (newValue)
 						TMPBar.savedVariables.showBankBag = newValue
 						TMPBarWindowBankSpaceLabel:SetHidden(not newValue)
 					end,
 		},
-		[4] = {
-			type = "submenu",
-			name = "Colors",
-			tooltip = "Change bag colors.",
-			controls = {
-				[1] = {
-					type = "colorpicker",
-					name = "Normal color",
-					tooltip = "Changes the normal color of the bag text.",
-					getFunc = function () return unpack(TMPBar.savedVariables.normalColor) end,
-					setFunc = function (r, g, b, a)
-								TMPBar.savedVariables.normalColor = { r, g, b, a }
-								TMPBarWindowBagSpaceLabel:SetColor(r, g, b, a)
-							end,
-				},
-				[2] = {
-					type = "colorpicker",
-					name = "Warning color",
-					tooltip = "Changes the warning color of the bag text.",
-					getFunc = function () return unpack(TMPBar.savedVariables.warningColor) end,
-					setFunc = function (r, g, b, a)
-								TMPBar.savedVariables.warningColor = { r, g, b, a },
-								TMPBarWindowBagSpaceLabel:SetColor(r, g, b, a)
-							end,
-				},
-				[3] = {
-					type = "colorpicker",
-					name = "Full color",
-					tooltip = "Changes the color of the bag text when bags are full.",
-					getFunc = function () return unpack(TMPBar.savedVariables.fullColor) end,
-					setFunc = function (r, g, b, a)
-								TMPBar.savedVariables.fullColor = { r, g, b, a },
-								TMPBarWindowBagSpaceLabel:SetColor(r, g, b, a)
-							end,
-				}
-			}
+		[7] = {
+			type = "colorpicker",
+			name = "Normal color",
+			tooltip = "Changes the normal color of the bag text.",
+			getFunc = function ()
+						return unpack(TMPBar.savedVariables.normalColor)
+					end,
+			setFunc = function (r, g, b, a)
+						TMPBar.savedVariables.normalColor = { r, g, b, a }
+					end,
 		},
-		[5] = {
+		[8] = {
+			type = "colorpicker",
+			name = "Warning color",
+			tooltip = "Changes the warning color of the bag text.",
+			getFunc = function ()
+						return unpack(TMPBar.savedVariables.warningColor)
+					end,
+			setFunc = function (r, g, b, a)
+						TMPBar.savedVariables.warningColor = { r, g, b, a }
+					end,
+		},
+		[9] = {
+			type = "colorpicker",
+			name = "Full color",
+			tooltip = "Changes the color of the bag text when bags are full.",
+			getFunc = function ()
+						return unpack(TMPBar.savedVariables.fullColor)
+					end,
+			setFunc = function (r, g, b, a)
+						TMPBar.savedVariables.fullColor = { r, g, b, a }
+					end,
+		},
+		[10] = {
 			type = "dropdown",
 			name = "Warning",
 			tooltip = "Choose when to set the warningcolor on the bag text",
 			choices = { 5, 10, 15, 20 },
-			getFunc = function () return TMPBar.savedVariables.warningWhen end,
+			getFunc = function ()
+						return TMPBar.savedVariables.warningWhen
+					end,
 			setFunc = function (newValue)
 						TMPBar.savedVariables.warningWhen = newValue
 					end,
